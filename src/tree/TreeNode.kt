cb225c1b3e3928c111f6528be1658428f68bca9e
zak <linhenji@163.com>
2021-03-15 23:43:22 +0800

    feat: 添加二叉树节点和 kotlin 依赖

---
+++ src/tree/TreeNode.kt
@@ -0,0 +1,12 @@
+package tree
+
+/**
+ * 二叉树节点实例
+ *
+ * @param value 节点存的值
+ * @param left  指向左侧子节点的指针
+ * @param right 执行右侧子节点的指针
+ */
+class BinaryTreeNode(var value: Int = 0,
+                     val left: BinaryTreeNode? = null,
+                     val right: BinaryTreeNode? = null)
