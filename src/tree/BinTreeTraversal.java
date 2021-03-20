d2f05a034ef64821987256afaba7cbd4aa4c8f82
zak <linhenji@163.com>
2021-03-20 20:10:12 +0800

    feat: 987.二叉树垂序遍历

--- src/tree/BinTreeTraversal.java
+++ src/tree/BinTreeTraversal.java
@@ -2,6 +2,7 @@ package tree;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -42,10 +43,13 @@ public class BinTreeTraversal {
         System.out.println();
         System.out.println("二叉树锯齿形层序遍历结果如下：");
         jaggedOrder(root);
+        System.out.println();
+        System.out.println("二叉树垂序遍历结果如下：");
+        verticalTraversal(root);
     }
 
     /**
-     * 前序遍历
+     * 144.前序遍历
      *
      * @param treeNode treeNode
      */
@@ -59,7 +63,7 @@ public class BinTreeTraversal {
     }
 
     /**
-     * 中序遍历
+     * 94.中序遍历
      *
      * @param treeNode treeNode
      */
@@ -73,7 +77,7 @@ public class BinTreeTraversal {
     }
 
     /**
-     * 后序遍历
+     * 145.后序遍历
      *
      * @param treeNode treeNode
      */
@@ -87,7 +91,7 @@ public class BinTreeTraversal {
     }
 
     /**
-     * 自顶向下的层序遍历
+     * 102.自顶向下的层序遍历
      *
      * @param treeNode treeNode
      */
@@ -116,7 +120,7 @@ public class BinTreeTraversal {
     }
 
     /**
-     * 自底向上的层序遍历
+     * 107.自底向上的层序遍历
      *
      * @param treeNode treeNode
      */
@@ -150,7 +154,7 @@ public class BinTreeTraversal {
 
 
     /**
-     * 二叉树的锯齿形层序遍历
+     * 103.二叉树的锯齿形层序遍历
      *
      * @param treeNode treeNode
      */
@@ -187,6 +191,83 @@ public class BinTreeTraversal {
         for (List<Integer> group : result) {
             for (Integer item : group) {
                 System.out.print(item + " ");
+            }
+        }
+    }
+
+    static List<Location> locations;
+
+    /**
+     * 987.二叉树垂序遍历
+     *
+     * @param root root
+     */
+    public static void verticalTraversal(BinaryTreeNode root) {
+        locations = new ArrayList<>();
+        getFrontTraversal(root, 0, 0);
+        Collections.sort(locations);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+
+        int prev = locations.get(0).x;
+
+        for (Location loc: locations) {
+            // If the x value changed, it's part of a new report.
+            if (loc.x != prev) {
+                prev = loc.x;
+                ans.add(new ArrayList<>());
+            }
+            // We always add the node's value to the latest report.
+            ans.get(ans.size() - 1).add(loc.value);
+        }
+
+        for (List<Integer> group : ans) {
+            for (Integer item : group) {
+                System.out.print(item + " ");
+            }
+        }
+    }
+
+    /**
+     * 前序遍历记录节点坐标
+     *
+     * @param node   node
+     * @param nodeX  nodeX
+     * @param nodeY  nodeY
+     */
+    public static void getFrontTraversal(BinaryTreeNode node, int nodeX, int nodeY) {
+        if (locations != null &&
+                node != null) {
+            locations.add(new Location(nodeX, nodeY, node.getValue()));
+            getFrontTraversal(node.getLeft(), nodeX - 1, nodeY + 1);
+            getFrontTraversal(node.getRight(), nodeX + 1, nodeY + 1);
+        }
+    }
+
+    /**
+     * 节点坐标
+     */
+    static class Location implements Comparable<Location> {
+        int x;
+        int y;
+        int value;
+
+        Location(int x, int y, int value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+
+
+        @Override
+        public int compareTo(Location that) {
+            if (this.x != that.x) {
+                return Integer.compare(this.x, that.x);
+            } else if (this.y != that.y) {
+                return Integer.compare(this.y, that.y);
+            } else {
+                return Integer.compare(this.value, that.value);
             }
         }
     }
