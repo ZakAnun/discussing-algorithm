9a24089e90ab6989da9772d523610a4e8bfc9dee
cm <liminglin@codemao.cn>
2021-03-30 19:04:07 +0800

    feat: 897.递增顺序查找树

--- src/tree/BinTreePractice.java
+++ src/tree/BinTreePractice.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.List;
+
 public class BinTreePractice {
 
     public static void main(String[] args) {
@@ -13,6 +15,20 @@ public class BinTreePractice {
         System.out.println("606.根据二叉树创建字符串");
         System.out.println("606_1: " + tree2Str(demoTree606_1));
         System.out.println("606_2: " + tree2Str(demoTree606_2));
+
+        BinaryTreeNode demoTree897 = new BinaryTreeNode(5,
+                new BinaryTreeNode(3,
+                        new BinaryTreeNode(2, new BinaryTreeNode(1, null, null), null),
+                        new BinaryTreeNode(4, null, null)),
+                new BinaryTreeNode(6, null, new BinaryTreeNode(8,
+                        new BinaryTreeNode(7, null, null),
+                        new BinaryTreeNode(9, null, null))));
+        System.out.println("897. 递增顺序查找树");
+        System.out.print("中序遍历结果: ");
+        BinaryTreeNode result897 = increasingBST(demoTree897);
+        System.out.println();
+        System.out.print("结果树前序遍历结果: ");
+        BinTreeTraversal.inOrderTraversal(result897, null);
     }
 
     /**
@@ -50,5 +66,43 @@ public class BinTreePractice {
         }
 
         return result;
+    }
+
+    /**
+     * 897.递增顺序查找树
+     * 给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。
+     *
+     * @param root 树的根节点
+     *
+     * @return 结果树
+     */
+    public static BinaryTreeNode increasingBST(BinaryTreeNode root) {
+        List<Integer> inOrderList = BinTreeTraversal.inOrderTraversalWithResult(root);
+//        return buildTree(inOrderList); // 递归的方式
+        BinaryTreeNode newRoot = new BinaryTreeNode(0, null, null);
+        BinaryTreeNode curRoot = newRoot;
+        for (Integer item : inOrderList) {
+            if (curRoot != null) {
+                curRoot.setRight(new BinaryTreeNode(item, null, null));
+                curRoot = curRoot.getRight();
+            }
+        }
+        return newRoot.getRight();
+    }
+
+    /**
+     * 879.根据中序遍历构建结果树
+     *
+     * @param result 中序遍历
+     * @return 结果树
+     */
+    private static BinaryTreeNode buildTree(List<Integer> result) {
+        if (result == null || result.size() == 0) {
+            return null;
+        }
+        BinaryTreeNode root = new BinaryTreeNode(result.get(0), null, null);
+        result.remove(result.get(0));
+        root.setRight(buildTree(result));
+        return root;
     }
 }
