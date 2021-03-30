daa579b0d306da0a64458fe3d28eeb140448bff4
zak <linhenji@163.com>
2021-03-31 00:33:25 +0800

    feat: 429.N 叉树的层序遍历

--- src/tree/TreeTraversal.java
+++ src/tree/TreeTraversal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 树的遍历（N 叉树）
@@ -24,6 +25,18 @@ public class TreeTraversal {
         demoTree559Second.setChildren(childrenGroup3);
         System.out.print("559.N 叉树的最大深度: " + maxDepth(demoTree559));
         System.out.println();
+        System.out.println("429.N 叉树的层序遍历如下: ");
+        Node demoTree429 = demoTree559;
+        List<List<Integer>> result = levelOrder(demoTree429);
+        for (List<Integer> group : result) {
+            System.out.print("[");
+            for (Integer item : group) {
+                System.out.print(item + " ");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+        System.out.println("429.N 叉树的层序遍历结束");
     }
 
     /**
@@ -44,5 +57,42 @@ public class TreeTraversal {
             childNodeList.add(maxDepth(child));
         }
         return Collections.max(childNodeList) + 1;
+    }
+
+    /**
+     * 429.N 叉树的层序遍历
+     * 给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）
+     *
+     * @param root N 叉树根节点
+     *
+     * @return N 叉树层序遍历结果集
+     */
+    public static List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int curSize = queue.size();
+            List<Integer> group = new ArrayList<>();
+            for (int i = 0; i < curSize; ++i) {
+                Node node = queue.poll();
+                if (node != null) {
+                    group.add(node.getValue());
+                    if (node.getChildren() != null) {
+                        for (Node child : node.getChildren()) {
+                            queue.offer(child);
+                        }
+                    }
+                }
+            }
+            result.add(group);
+        }
+        return result;
     }
 }
