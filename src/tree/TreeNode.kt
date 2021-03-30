e683f0c697d754844ea7ec34b814695a3676b888
zak <linhenji@163.com>
2021-03-31 00:22:28 +0800

    feat: 添加N叉树实体类

--- src/tree/TreeNode.kt
+++ src/tree/TreeNode.kt
@@ -10,3 +10,12 @@ package tree
 class BinaryTreeNode(var value: Int = 0,
                      var left: BinaryTreeNode? = null,
                      var right: BinaryTreeNode? = null)
+
+/**
+ * 树节点实例
+ *
+ * @param value     节点存的值
+ * @param children  节点的子节点
+ */
+class Node(var value: Int = 0,
+           var children: List<Node>? = null)
