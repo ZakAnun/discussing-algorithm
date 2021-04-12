afab55332d8bbc2de204a1c3d0a2dd819e1aa05a
zak <linhenji@163.com>
2021-04-12 23:09:59 +0800

    feat: 617. 合并二叉树

--- src/tree/BinTreeCreation.java
+++ src/tree/BinTreeCreation.java
@@ -13,6 +13,7 @@ public class BinTreeCreation {
         int[] preOrder = new int[] {3, 9, 20, 15, 7};
         int[] inOrder = new int[] {9, 3, 15, 20, 7};
         int[] postOrder = new int[] {9, 15, 7, 20, 3};
+
         System.out.println("======== 105 ========");
         BinaryTreeNode result105 = buildTreeWithPreOrderAndInOrder(preOrder, inOrder);
         System.out.println("根据前序遍历和中序遍历序列得出构造的结果树为: ");
@@ -27,6 +28,7 @@ public class BinTreeCreation {
         System.out.println();
         System.out.println("======== 105 ========");
         System.out.println();
+
         System.out.println("======== 106 ========");
         BinaryTreeNode result106 = buildTreeWithInOrderAndPostOrder(inOrder, postOrder);
         System.out.println("根据中序遍历和后序遍历序列得出构造的结果树为: ");
@@ -100,6 +102,26 @@ public class BinTreeCreation {
         BinTreeTraversal.preOrderTraversal(resultOffer27, null);
         System.out.println();
         System.out.println("======== 剑指 Offer 27 ========");
+        System.out.println();
+
+        System.out.println("======== 617. 合并二叉树 ========");
+        BinaryTreeNode demo617One = new BinaryTreeNode(1,
+                new BinaryTreeNode(3,
+                        new BinaryTreeNode(5, null, null),
+                        null),
+                new BinaryTreeNode(2, null, null));
+        BinaryTreeNode demo617Two = new BinaryTreeNode(2,
+                new BinaryTreeNode(1,
+                        null,
+                        new BinaryTreeNode(4, null, null)),
+                new BinaryTreeNode(3,
+                        null,
+                        new BinaryTreeNode(7, null, null)));
+        BinaryTreeNode result617 = mergeTrees(demo617One, demo617Two);
+        System.out.print("合并两个 demo 二叉树的结果树的前序遍历为: ");
+        BinTreeTraversal.preOrderTraversal(result617, null);
+        System.out.println();
+        System.out.println("======== 617. 合并二叉树 ========");
         System.out.println();
     }
 
@@ -331,5 +353,30 @@ public class BinTreeCreation {
                 mirrorTree(root.getLeft()));
 
         return newRoot;
+    }
+
+    /**
+     * 617. 合并二叉树
+     *
+     * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+     *
+     * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，
+     * 否则不为 NULL 的节点将直接作为新二叉树的节点。
+     *
+     * @param root1 树1
+     * @param root2 树2
+     * @return 合并两棵树后的结果树
+     */
+    public static BinaryTreeNode mergeTrees(BinaryTreeNode root1, BinaryTreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        return new BinaryTreeNode(root1.getValue() + root2.getValue(),
+                mergeTrees(root1.getLeft(), root2.getLeft()),
+                mergeTrees(root1.getRight(), root2.getRight()));
     }
 }
