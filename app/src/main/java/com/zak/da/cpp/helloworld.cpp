aef23832bda4a1b2ff9db7e69a98f1c08f2ba25f
liminglin <liminglin@youzan.com>
2022-04-20 10:11:43 +0800

    feat: 添加 cpp 文件

---
+++ app/src/main/java/com/zak/da/cpp/helloworld.cpp
@@ -0,0 +1,15 @@
+//
+// Created by zak on 2022/4/20.
+//
+
+#include<iostream>
+
+/**
+ * in mac os, run g++ -o outputName *.cpp to generate executable file to figure out the result.
+ * outputName 生成的文件名称
+ * * 表示希望编译的 cpp 文件
+ */
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
